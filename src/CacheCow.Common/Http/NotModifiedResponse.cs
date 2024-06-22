372a423ed8f9fd5bdf35349304420290b5f7d0e2
Cédric Luthi <cedric.luthi@gmail.com>
2024-06-22 15:18:41 +0200

    [WIP] Cleanup dependencies and use FrameworkReference

--- src/CacheCow.Common/Http/NotModifiedResponse.cs
+++ src/CacheCow.Common/Http/NotModifiedResponse.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Text;
 
@@ -28,5 +27,5 @@ namespace CacheCow.Common.Http
 
 	}
 
-	
+
 }
